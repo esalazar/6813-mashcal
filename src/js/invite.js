de19e0e7fdeacf82fd805cce4d9b0a04d086970c
Ulziibayar Otgonbaatar <j2i2t2u2@gmail.com>
2012-04-15 15:06:29 -0400

    fixed invite.js and removed the script inside index.html

--- src/js/invite.js
+++ src/js/invite.js
@@ -1,11 +1,14 @@
 
-$(function() {
-	var availableTags = ["Brat","Bob", "Kate", "Tito"];
-    $('#search_box').autocomplete({
-      source: availableTags,
-		  target: $('#suggestions'),
-      link: 'testing.cfm?term=',
-		  minLength: 1
-    });
+$(document).ready(function (){	
+		
+	var availableTags = ["Bob", "Kate", "Tito", "Josh"];
+	
+	$("#searchField").autocomplete({
+		target: $('#suggestions'),
+		source: availableTags,
+		link: 'friend=',
+		minLength: 1,
+		//select: function(event, ui) { alert("");}
+	});
+	
 });
-
