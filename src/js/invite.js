a7beef0fcbdd484497f5183ee53eb72d10083e9e
Ulziibayar Otgonbaatar <j2i2t2u2@gmail.com>
2012-04-15 13:20:27 -0400

    Removed the conflict text from invite.js

--- src/js/invite.js
+++ src/js/invite.js
@@ -1,19 +1,4 @@
-<<<<<<< HEAD
 
-$(function(){
-	alert(document.getElementById("searchField"));
-	var availableTags = ["c++", "java", "php", "coldfusion", "javascript", "asp", "ruby"];
-	var suggestions = document.getElementById("suggestions");
-	document.getElementById("invite").bind("pageshow", function(e) {
-		document.getElementById("searchField").autocomplete({
-			target: suggestions,
-			source: availableTags,
-			link: 'testing.cfm?term=',
-			minLength: 1
-		});
-	});	
- });
-=======
 $(function() {
 	var availableTags = ["Brat","Bob", "Kate", "Tito"];
     $('#search_box').autocomplete({
@@ -24,4 +9,3 @@ $(function() {
     });
 });
 
->>>>>>> 6e5dd50d7e207090b666082be52ddbd959bf1d01
