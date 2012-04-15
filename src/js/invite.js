3d6a4476941ec06cc8c678ab0d96cecb02f45a9a
Ryan Lopopolo <rjl@hyperbo.la>
2012-04-15 06:57:05 -0400

    Get rid of this goddamned alert that does nothing but make my life miserable whenever I reload the page

--- src/js/invite.js
+++ src/js/invite.js
@@ -1,12 +1,12 @@
 $(function() {
 	/*
 	var button = document.getElementById('button');
-	
+
 	if (button.addEventListener){
-		button.addEventListener('click', function () {clickevent()}, false );	
+		button.addEventListener('click', function () {clickevent()}, false );
 	}
 	else {
-    	if (button.attachEvent) {  
+    	if (button.attachEvent) {
         	button.attachEvent ("onclick", function () {clickevent()});
         }
     }
@@ -14,26 +14,25 @@ $(function() {
 
     	var code = (e.keyCode ? e.keyCode : e.which);
     	//alert(code);
-  			if(code == 13) { 
+  			if(code == 13) {
 				clickevent();
-			       
+
   			}
 
 	});
-   
+
 	last_s = generaterRandomWord()	;
 	document.getElementById("spanish_word").innerHTML = last_s;
 	document.getElementById("spanish_word").setAttribute("class","bold") ;
-	
+
 	*/
-	alert("");
 	var availableTags = ["Brat","Bob", "Kate", "Tito"];
     $('#search_box').autocomplete({
         source: availableTags,
 		target: $('#suggestions'),
         link: 'testing.cfm?term=',
 		minLength: 1
-		
+
         //select: function (event, ui) { }
     });
 });
