21211ee4b445a22bcf1c5a4b89ff8af1fa18e4c5
Ryan Lopopolo <rjl@hyperbo.la>
2012-04-15 08:04:16 -0400

    Remove empty js files and commented out code.
    someone on this team is making me cry

--- src/js/invite.js
+++ src/js/invite.js
@@ -1,38 +1,10 @@
 $(function() {
-	/*
-	var button = document.getElementById('button');
-
-	if (button.addEventListener){
-		button.addEventListener('click', function () {clickevent()}, false );
-	}
-	else {
-    	if (button.attachEvent) {
-        	button.attachEvent ("onclick", function () {clickevent()});
-        }
-    }
-    $("#form_input").bind('keypress', function(e) {
-
-    	var code = (e.keyCode ? e.keyCode : e.which);
-    	//alert(code);
-  			if(code == 13) {
-				clickevent();
-
-  			}
-
-	});
-
-	last_s = generaterRandomWord()	;
-	document.getElementById("spanish_word").innerHTML = last_s;
-	document.getElementById("spanish_word").setAttribute("class","bold") ;
-
-	*/
 	var availableTags = ["Brat","Bob", "Kate", "Tito"];
     $('#search_box').autocomplete({
-        source: availableTags,
-		target: $('#suggestions'),
-        link: 'testing.cfm?term=',
-		minLength: 1
-
-        //select: function (event, ui) { }
+      source: availableTags,
+		  target: $('#suggestions'),
+      link: 'testing.cfm?term=',
+		  minLength: 1
     });
 });
+
