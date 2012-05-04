44d4d5c8daac4d394079fba59a7b845948564630
Ryan Lopopolo <rjl@hyperbo.la>
2012-05-04 16:52:20 -0400

    Use ajax to load the invite page

--- dev_server.rb
+++ dev_server.rb
@@ -4,16 +4,27 @@ require "sinatra"
 require "json"
 
 set :public_folder, File.join(File.dirname(__FILE__), "src")
+set :views, File.join(File.dirname(__FILE__), "src")
 
 set :port, 6813
 
-get /^\/[^\/]*$/ do
+get "/" do
   send_file("src/index.html")
 end
 
+get "/invite.html" do
+  @contacts = {}
+  @contacts["B"] = ["Bill Smith", "Bob Jones", "Billy Awesome", "Beyonce"]
+  @contacts["D"] = ["Dick Clark (RIP)"]
+  @contacts["K"] = ["Kate Upton", "Katy Perry"]
+  @contacts["J"] = ["Jennifer Anniston"]
+  @contacts["R"] = ["Rihanna"]
+  @contacts.each_pair { |k,v| @contacts[k] = v.sort }
+  @contact_letters = ["B", "D", "J", "K", "R"].sort
+  erb :invite
+end
+
 get "/ajax/contacts/:id" do
-  puts "here"
-  puts request.path_info
   contacts = {}
   contacts[:contacts] = ["Bill Smith", "Bob Jones", "Billy Awesome",
                          "Kate Upton", "Rihanna", "Dick Clark (RIP)",
--- src/js/display-contacts.js
+++ src/js/display-contacts.js
@@ -1,68 +1,18 @@
 var getContacts = function(id) {
-  console.log("here");
-  $("#listClone").remove();
-  $.get("/ajax/contacts/" + id, null, function(data, textStatus, jqXHR) {
-    var contacts = JSON.parse(data);
-    contacts = contacts.contacts;
-    contacts.sort();
-    var lastLetter = null;
-    var list = $("#suggestions");
-    var listClone = list.clone();
-    listClone.attr("id", "listClone");
-    listClone.html("");
-    for (var i=0; i < contacts.length; i++) {
-      var curr = contacts[i];
-      var firstLetter = curr.charAt(0).toUpperCase();
-      var last = false;
-      if (firstLetter !== lastLetter) {
-        lastLetter = firstLetter;
-        var divider = list.find(".suggestionsDivider");
-        divider = divider.clone();
-        divider.html(lastLetter);
-        listClone.append(divider);
-      }
-      if (i == contacts.length - 1) { last = true; }
-      else {
-        var next = contacts[i+1];
-        if (next.charAt(0).toUpperCase() !== lastLetter) {
-          last = true;
-        }
-      }
-      var neutral, sel;
-      if (last) {
-        neutral = $("#suggestions2").find(".suggestionsNeutral-last");
-        sel = list.find(".suggestionsSel-last");
+  $("#suggestions li").click(function() {
+    var current = this;
+    $("#suggestions li[data-contact='"+$(this).attr("data-contact")+"']").each(function() {
+    console.log("click")
+      if (this === current) {
+        $(this).addClass("invite-hidden");
       } else {
-        neutral = list.find(".suggestionsNeutral");
-        sel = list.find(".suggestionsSel");
+        $(this).removeClass("invite-hidden");
       }
-
-      neutral = neutral.clone();
-      sel = sel.clone();
-      neutral.attr("data-contact", curr);
-      neutral.find("a").html(curr);
-      neutral.find("span").remove();
-      sel.attr("data-contact", curr);
-      sel.find("a").html(curr);
-      sel.find("span").remove();
-      sel.css({ "display" : "none" })
-      listClone.append(neutral);
-      listClone.append(sel);
-    }
-    list.css({ "display" : "none" });
-    $("#suggestions2").css({ "display" : "none" });
-    $(list).after(listClone);
-    $("#listClone li").click(function() {
-      var current = this;
-      $("#listClone li[data-contact='"+$(this).attr("data-contact")+"']").each(function() {
-      console.log("click")
-        if (this === current) {
-          $(this).css({ "display" : "none" });
-        } else {
-          $(this).css({ "display" : "block" });
-        }
-      });
-      return false;
     });
+    return false;
   });
+};
+
+var goToInvite = function() {
+  $.mobile.changePage("invite.html");
 };
